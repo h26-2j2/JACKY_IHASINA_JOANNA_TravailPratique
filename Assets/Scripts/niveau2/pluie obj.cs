dbc754912cc7c770a41ec64b36f673288cbcbe0d
Jojo <e2566476@cmaisonneuve.qc.ca>
2026-05-06 15:54:47 -0400

    debut script

--- Assets/Scripts/niveau2/pluie obj.cs
+++ Assets/Scripts/niveau2/pluie obj.cs
@@ -5,7 +5,9 @@ public class pluieobj : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //parallax et aleatoire
+        //boolen est tombé si cliqué aqrrete et ombé et est déplacaable avec le clique
+
     }
 
     // Update is called once per frame
